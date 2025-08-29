9331a85412192e984864e002c1dbefd55444fd4e
Arif <muhammad.ariful98@gmail.com>
2025-08-29 17:56:32 +0600

    Project initialization complete

---
+++ src/routes/router.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+    element:<div>Hellow world</div>
+    }
+]);
+
+export default router;
